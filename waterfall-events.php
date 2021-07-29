367905a2a5e1a07be51e15d75cc3515ee19741d9
Michiel Tramper <michielsite@hotmail.com>
2021-07-29 12:11:14 +0200

    New version

--- waterfall-events.php
+++ waterfall-events.php
@@ -3,7 +3,7 @@
 Plugin Name:  Waterfall Events
 Plugin URI:   https://www.makeitwork.press/wordpress-plugins/waterfall-events/
 Description:  The Waterfall Events plugin upgradres your Waterfall WordPress theme with event capabilities.
-Version:      0.0.4
+Version:      0.0.5
 Author:       Make it WorkPress
 Author URI:   https://makeitwork.press/
 License:      GPL3
